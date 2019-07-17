ed7c78802260bd3a7275ac507abc18a6c772fd39
Roj JC Carranza <roj_jc_carranza@mfcgd.com>
2019-07-17 16:57:42 +0800

    Prototype Pattern

---
+++ src/prototype/PrototypeDemo.java
@@ -0,0 +1,27 @@
+package prototype;
+
+public class PrototypeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Registry registry = new Registry();
+		Movie movie = (Movie) registry.createItem("Movie");
+		movie.setTitle("Creational Patterns - Prototype in Java");
+		
+		System.out.println(movie);
+		System.out.println(movie.getRuntime());
+		System.out.println(movie.getTitle());
+		System.out.println(movie.getUrl());
+
+		Movie anotherMovie = (Movie) registry.createItem("Movie");
+		anotherMovie.setTitle("Update Creational Patterns - Prototype in Java");
+		
+		System.out.println(anotherMovie);
+		System.out.println(anotherMovie.getRuntime());
+		System.out.println(anotherMovie.getTitle());
+		System.out.println(anotherMovie.getUrl());
+
+	}
+
+}
---
+++ src/prototype/Registry.java
@@ -0,0 +1,44 @@
+package prototype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Registry {
+	
+	private Map<String, Item> items = new HashMap<String, Item>();
+	
+	public Registry() {
+		this.loadItems();
+	}
+	
+	public Item createItem(String type) {
+		Item item = null;
+		
+		try {
+			item = (Item) items.get(type).clone();
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return item; 
+	}
+	
+	
+	private void loadItems() {
+		Movie movie = new Movie();
+		movie.setTitle("Basic Movie");
+		movie.setPrice(24.99);
+		movie.setRuntime("2 hrs");
+		items.put("Movie", movie);
+		
+		Book book = new Book();
+		book.setTitle("Basic Book");
+		book.setNumberOfPages(335);
+		book.setPrice(99.9);
+		items.put("Book", book);
+	}
+	
+	
+
+}
